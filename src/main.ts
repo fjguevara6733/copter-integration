4b91b365efaf36cb3d6cb29ee948dcf568e3fdd2
martinc <martin.developer25@gmail.com>
2024-07-02 17:18:09 -0300

    cambiar de puerto

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   const globalPrefix = 'api';
 
   const config = new DocumentBuilder()
